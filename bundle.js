cd796720740095452b7af175c7b933befc719bf1
Sidicley Schneider <sidi.fev@gmail.com>
2020-08-02 23:16:57 -0300

    create class TaskList and constructor methods

--- bundle.js
+++ bundle.js
@@ -1,3 +1,14 @@
 "use strict";
 
-alert("teste");
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var tasklist = function tasklist() {
+  _classCallCheck(this, tasklist);
+
+  this.tituloInput = document.getElementById('titleInput');
+  this.mensagemInput = document.getElementById('messageField');
+  this.adicionar = document.getElementById('bnt');
+  this.caixaRecados = document.getElementById('caixa-recados'); // console.log(this.tituloInput, this.mensagemInput, this.adicionar);
+};
+
+new tasklist();
--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-alert("teste");
+class tasklist {
+  constructor() {
+    this.tituloInput = document.getElementById('titleInput');
+    this.mensagemInput = document.getElementById('messageField');
+    this.adicionar = document.getElementById('bnt');
+    this.caixaRecados = document.getElementById('caixa-recados')
+
+    // console.log(this.tituloInput, this.mensagemInput, this.adicionar);
+  }
+}
+
+new tasklist();
